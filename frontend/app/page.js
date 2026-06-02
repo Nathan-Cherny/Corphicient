177bc81b7f14732946034213ff1ea5f3c48ef0b1
Nathan Cherny <natec3632@gmail.com>
2026-06-02 07:51:16 -0400

    pokemon! at the top!

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -3,14 +3,14 @@
 import Form from "./components/forms/Forms";
 import { addSong } from "./components/songs/SongFunctions";
 import { addPlaylist } from "./components/playlists/PlaylistFunctions";
-import SongsList from "./components/songs/SongsList";
 
-import { useEffect, useState } from "react";
 import PlaylistList from "./components/playlists/PlaylistList";
+import Pokemon from "./components/pokemon/getPokemon";
 
 export default function Home() {
   return (
     <div>
+      <Pokemon/>
       <PlaylistList />
       <Form formType="get_song_form" nonFormFields={["secondsPlayed", "src"]} submitFunction={addSong}/>
       <Form formType="get_playlist_form" submitFunction={addPlaylist}/>
