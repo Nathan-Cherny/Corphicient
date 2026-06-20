7f24f87a3237f2e971525e05eff6722e6653968b
Nathan Cherny <natec3632@gmail.com>
2026-06-20 19:39:20 -0400

    working on songedit

---
+++ frontend/app/components/songs/EditSong.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function EditSong({}) {
+  return (
+    <div className="flex flex-col gap-3 bg-white p-5">
+      <p>test</p>
+    </div>
+  );
+}
--- frontend/app/components/songs/SongCard.jsx
+++ frontend/app/components/songs/SongCard.jsx
@@ -1,6 +1,9 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as Song from "./SongFunctions";
+import { Edit, Eraser } from "lucide-react";
+import FadeOverlay from "../layout/FadeOverlay";
 import { useNotification } from "../layout/notification/NotificationContext";
+import EditSong from './EditSong'
 
 export default function SongCard({
   song,
@@ -11,6 +14,7 @@ export default function SongCard({
 }) {
   const audioRef = useRef(null);
   const notify = useNotification();
+  const [showEdit, setShowEdit] = useState(false);
 
   useEffect(() => {
     if (isCurrentSong) {
@@ -23,7 +27,7 @@ export default function SongCard({
   return (
     <div
       onClick={() => setCurrentSong(isCurrentSong ? null : song)}
-      style={{backgroundColor: "rgba(0, 0, 0, 0.15)"}}
+      style={{ backgroundColor: "rgba(0, 0, 0, 0.15)" }}
       className={`relative w-1/4 flex flex-col border transition-all duration-200 justify-between ${
         isCurrentSong
           ? "border-blue-500 border-4 shadow-lg shadow-blue-200"
@@ -40,7 +44,24 @@ export default function SongCard({
       >
         X
       </button> */}
-      <h3 className="font-bold text-3xl text-center m-5 h-full flex items-center justify-center">{song.name}</h3>
+
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          e.preventDefault();
+        }}
+        className="absolute top-1 left-1 text-black w-4 h-4 flex items-center justify-center hover:scale-110 hover:cursor-pointer transition-all duration-200"
+      >
+        <Edit />
+      </button>
+
+      <FadeOverlay isOpen={showEdit} onClose={() => setShowEdit(false)}>
+        <EditSong/>
+      </FadeOverlay>
+
+      <h3 className="font-bold text-3xl text-center m-5 h-full flex items-center justify-center">
+        {song.name}
+      </h3>
       <audio
         className="w-full"
         ref={(el) => {
