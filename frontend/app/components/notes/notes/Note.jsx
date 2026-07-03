a41ebab2537963f8e08e854f9677178953c9d68f
Nathan Cherny <natec3632@gmail.com>
2026-07-02 22:20:20 -0400

    redownloaded songs

--- frontend/app/components/notes/notes/Note.jsx
+++ frontend/app/components/notes/notes/Note.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 export default function Note({ note }) {
 
   return (
-    <div className="border">
+    <div className="">
         <p>{note.name}</p>
         <p>{note.note}</p>
     </div>
