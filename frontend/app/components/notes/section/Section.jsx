1407898e418d5b0dac64e4dac5fb4fce2b6201ce
Nathan Cherny <natec3632@gmail.com>
2026-07-26 13:12:15 -0400

    change title section

--- frontend/app/components/notes/section/Section.jsx
+++ frontend/app/components/notes/section/Section.jsx
@@ -29,7 +29,7 @@ export default function Section({ section, setUpdate, collapse }) {
     <div
       style={{ backgroundColor: section.color }}
       onClick={(e) => {
-        if(!e.ctrlKey) return
+        if (!e.ctrlKey) return;
         setCollapsed(!collapsed);
       }}
       className={`p-5 shadow-2xl rounded-2xl h-full border relative cursor-pointer`}
@@ -45,26 +45,26 @@ export default function Section({ section, setUpdate, collapse }) {
             submitFunction={(e) => {
               addNoteToSection(e, section.id);
               notify({ message: `Added Note` });
-              setUpdate(prev => prev+1)
+              setUpdate((prev) => prev + 1);
             }}
             name={`Add Note To ${section.name}`}
           />
         </div>
       </FadeOverlay>
 
-      <FadeOverlay
-        isOpen={colorMenuOpen}
-        onClose={() => setColorMenuOpen(false)}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          SectionFns.updateSection(e, section.id);
+          setUpdate((prev) => prev + 1);
+        }}
       >
-        <div className="bg-white p-5 flex flex-col items-center">
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              e.stopPropagation()
-              SectionFns.updateSection(e, section.id);
-              setUpdate(prev => prev+1)
-            }}
-          >
+        <FadeOverlay
+          isOpen={colorMenuOpen}
+          onClose={() => setColorMenuOpen(false)}
+        >
+          <div className="bg-white p-5 flex flex-col items-center">
             <h1 className="text-xl">Set Color For Section</h1>
             <div className="flex flex-row gap-5 items-center">
               <input
@@ -80,57 +80,69 @@ export default function Section({ section, setUpdate, collapse }) {
               <p id="colorLabel">{section.color}</p>
             </div>
             <input type="submit" value={"Submit"} />
-          </form>
-        </div>
-      </FadeOverlay>
-      
-      <button
-        onClick={(e) => {
-          e.preventDefault()
-          e.stopPropagation()
-          setAddNoteMenuOpen(true);
-        }}
-        className="absolute top-2 right-2 border border-black bg-blue-500 text-white w-6 h-6 flex items-center justify-center hover:scale-110 hover:cursor-pointer transition-all duration-200"
-      >
-        <PlusIcon />
-      </button>
+          </div>
+        </FadeOverlay>
 
-      <button
-        type="button"
-        onClick={(e) => {
-          e.stopPropagation();
-          if (!confirm(`Delete Section ${section.name}?`)) return;
-          SectionFns.deleteSection(section.id);
-          setUpdate(prev => prev+1)
-          notify({ message: `Deleted Section` });
-        }}
-        className="absolute top-2 right-10 border border-black bg-red-500 text-white w-6 h-6 flex items-center justify-center hover:scale-110 hover:cursor-pointer transition-all duration-200"
-      >
-        X
-      </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            setAddNoteMenuOpen(true);
+          }}
+          className="absolute top-2 right-2 border border-black bg-blue-500 text-white w-6 h-6 flex items-center justify-center hover:scale-110 hover:cursor-pointer transition-all duration-200"
+        >
+          <PlusIcon />
+        </button>
 
-      <button
-        onClick={(e) => {
-          setColorMenuOpen(true);
-          e.stopPropagation()
-        }}
-        className="absolute top-2 right-18 border border-black bg-green-500 text-white w-6 h-6 p-0.5 flex items-center justify-center hover:scale-110 hover:cursor-pointer transition-all duration-200"
-      >
-        <Palette />
-      </button>
+        <button
+          type="button"
+          onClick={(e) => {
+            e.stopPropagation();
+            if (!confirm(`Delete Section ${section.name}?`)) return;
+            SectionFns.deleteSection(section.id);
+            setUpdate((prev) => prev + 1);
+            notify({ message: `Deleted Section` });
+          }}
+          className="absolute top-2 right-10 border border-black bg-red-500 text-white w-6 h-6 flex items-center justify-center hover:scale-110 hover:cursor-pointer transition-all duration-200"
+        >
+          X
+        </button>
 
-      <button
-        onClick={(e) => {
-          setCollapsed(!collapsed);
-        }}
-        className="absolute top-2 right-26 border border-black bg-orange-500 text-white w-6 h-6 p-0.5 flex items-center justify-center hover:scale-110 hover:cursor-pointer transition-all duration-200"
-      >
-        {collapsed ? <ChevronUp/> : <ChevronDown/>}
-      </button>
+        <button
+          onClick={(e) => {
+            setColorMenuOpen(true);
+            e.stopPropagation();
+          }}
+          className="absolute top-2 right-18 border border-black bg-green-500 text-white w-6 h-6 p-0.5 flex items-center justify-center hover:scale-110 hover:cursor-pointer transition-all duration-200"
+        >
+          <Palette />
+        </button>
 
-      <h1 className="text-3xl mb-5">{section.name}</h1>
+        <button
+          onClick={(e) => {
+            e.preventDefault()
+            e.stopPropagation()
+            setCollapsed(!collapsed);
+          }}
+          className="absolute top-2 right-26 border border-black bg-orange-500 text-white w-6 h-6 p-0.5 flex items-center justify-center hover:scale-110 hover:cursor-pointer transition-all duration-200"
+        >
+          {collapsed ? <ChevronUp /> : <ChevronDown />}
+        </button>
 
-      {(collapsed ) && <NoteList notes={section.notes}></NoteList>}
+        <input
+          className="text-3xl mb-1 w-11/12 text-center"
+          defaultValue={section.name}
+          name="name"
+          onKeyDown={(e) => {
+            if (e.key == "Enter" && !e.shiftKey) {
+              e.preventDefault();
+              e.currentTarget.form?.requestSubmit();
+            }
+          }}
+        />
+      </form>
+
+      {collapsed && <NoteList notes={section.notes}></NoteList>}
     </div>
   );
 }
