d016f4e32d920674a8263149dc858a425d753cee
Nathan Cherny <natec3632@gmail.com>
2026-07-26 15:01:05 -0400

    oops it's actually blank=True for note not section

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Note(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, blank=True)
     note = models.CharField(max_length=1000, blank=True)
     
     def __str__(self):
