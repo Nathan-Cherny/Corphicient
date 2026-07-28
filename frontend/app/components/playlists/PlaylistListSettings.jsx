9214e01d47bc7467d97b5a04739cc45e9fa33483
Nathan Cherny <natec3632@gmail.com>
2026-07-28 07:12:19 -0400

    trying to make setttings look better

--- frontend/app/components/playlists/PlaylistListSettings.jsx
+++ frontend/app/components/playlists/PlaylistListSettings.jsx
@@ -30,21 +30,25 @@ export default function PlaylistListSettings({settings, setSettings}) {
 
 function SettingsMenu({settings, setSettings}) {
   return (
-    <div className="p-15 bg-white flex flex-col items-center gap-5">
-      <h1 className="text-3xl">Settings</h1>
-      <Form
-        formType="get_song_form"
-        nonFormFields={["secondsPlayed", "src", "duration"]}
-        submitFunction={addSong}
-        name={"Add Song"}
-      />
-      <Form
-        formType="get_playlist_form"
-        submitFunction={addPlaylist}
-        name={"Add Playlist"}
-      />
-      <h1>Timeskip</h1>
-      <input type="number" onChange={(e) => {settings.timeSkip = e.target.valueAsNumber; setSettings(settings)}} defaultValue={settings.timeSkip}></input>
+    <div className="p-15 bg-white">
+      <h1 className="text-2xl text-center">Settings</h1>
+      <div className="bg-black/15 flex flex-col gap-5 items-stretch p-5 mt-5">
+        <Form
+          formType="get_song_form"
+          nonFormFields={["secondsPlayed", "src", "duration"]}
+          submitFunction={addSong}
+          name={"Add Song"}
+        />
+        <Form
+          formType="get_playlist_form"
+          submitFunction={addPlaylist}
+          name={"Add Playlist"}
+        />
+        <div className="flex flex-col border p-5">
+          <h1>Timeskip</h1>
+          <input type="number" onChange={(e) => {settings.timeSkip = e.target.valueAsNumber; setSettings(settings)}} defaultValue={settings.timeSkip}></input>
+        </div>
+      </div>
     </div>
   );
 }
