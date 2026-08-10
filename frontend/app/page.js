fef8098c81bcc192b91d0b81373c9d12509df07c
Nathan Cherny <natec3632@gmail.com>
2026-08-10 12:45:29 -0400

    google calendar in homepage

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -25,7 +25,7 @@ export default function Home() {
         <h1 className="text-5xl text-center font-light">{new Date().toLocaleDateString()}</h1>
         <hr className="my-10 w-full"/>
         
-        <div className="flex flex-row gap-15">
+        <div className="flex flex-row items-stretch gap-15 *:h-full">
           {sections.filter(s => s.name == "Today").map((s) => (
             <div className="w-full h-full" key={s.id}>
               <Section setUpdate={setUpdate} section={s} collapse={true} />
@@ -37,7 +37,14 @@ export default function Home() {
             <div className="border h-full">
             </div>
           </div>
+
+          <div className="flex flex-col border text-center rounded-2xl bg-amber-400 shadow-2xl p-5 w-fit">
+            <h1 className="text-3xl mb-5">Tasks</h1>
+            <iframe src="https://calendar.google.com/calendar/embed?src=natec3632%40gmail.com&ctz=America%2FNew_York&mode=AGENDA" width="400" height="600"></iframe>
+          </div>
         </div>
+
+
       </div>
     </PageMain>
   );
