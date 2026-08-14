f73395edfca69493b6ace5a2a2c319780d0100b5
Nathan Cherny <natec3632@gmail.com>
2026-08-14 11:52:39 -0400

    media metadata songs

--- frontend/app/components/songs/SongsList.jsx
+++ frontend/app/components/songs/SongsList.jsx
@@ -63,28 +63,28 @@ export default function SongsList({
 
   const hotkeysMap = {
     s: (e, audio) => {
-      if(!audio) return
+      if (!audio) return;
       playNextSong();
     },
     0: (e, audio) => {
-      if(!audio) return
+      if (!audio) return;
       audio.currentTime = 0;
     },
     ArrowRight: (e, audio) => {
-      if(!audio) return
+      if (!audio) return;
       audio.currentTime += timeSkip;
     },
     ArrowLeft: (e, audio) => {
-      if(!audio) return
+      if (!audio) return;
       audio.currentTime -= timeSkip;
     },
     l: (e, audio) => {
-      if(!audio) return
+      if (!audio) return;
       if (audio.loop) audio.loop = false;
       else audio.loop = true;
     },
     " ": (e, audio) => {
-      if(!audio) return
+      if (!audio) return;
       e.preventDefault();
       togglePause(audio, notify);
     },
@@ -102,6 +102,17 @@ export default function SongsList({
         duration: audio.duration || 0,
       });
     };
+
+    navigator.mediaSession.metadata = new MediaMetadata({
+      title: currentSong.name,
+      artwork: [
+        {
+          src: currentSong.thumbnail,
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+    });
 
     audio.addEventListener("timeupdate", updateProgress);
     audio.addEventListener("loadedmetadata", updateProgress);
@@ -127,6 +138,16 @@ export default function SongsList({
     window.addEventListener("keydown", handleKeyDown);
     return () => window.removeEventListener("keydown", handleKeyDown);
   }, [currentSong, getRandomSong]);
+
+  useEffect(() => {
+    navigator.mediaSession.setActionHandler("previoustrack", (e) => {
+      hotkeysMap["0"](e, currentAudioRef);
+    });
+
+    navigator.mediaSession.setActionHandler("nexttrack", () => {
+      playNextSong();
+    });
+  }, []);
 
   return (
     <div className="flex flex-wrap flex-col justify-center gap-7 ">
@@ -176,8 +197,12 @@ function CurrentSongInfo({
   return (
     <div>
       {/* Current Song Info */}
-      <div className={`flex flex-row justify-around items-stretch mb-5 *:text-center`}>
-        <div className={`${currentSong ? "bg-linear-to-r rounded-xl from-orange-500  via-green-500 to-purple-500 p-1" : "p-1"}`}>
+      <div
+        className={`flex flex-row justify-around items-stretch mb-5 *:text-center`}
+      >
+        <div
+          className={`${currentSong ? "bg-linear-to-r rounded-xl from-orange-500  via-green-500 to-purple-500 p-1" : "p-1"}`}
+        >
           <img
             onClick={(e) => hotkeysMap[" "](e, currentAudioRef.current)}
             className={`w-150 h-100 p-1 bg-gray-600 object-contain border-black border shadow-2xl rounded-xl cursor-pointer`}
@@ -190,10 +215,25 @@ function CurrentSongInfo({
         </div>
         <div className="flex flex-col gap-5 justify-evenly items-center w-100">
           <h1 className="text-3xl">
-            Playing <b style={{color: currentSong ? `rgb(${currentSong?.color.split(",").map(c => parseInt(c) - 75).join(",")})` : "black"}}>{currentSong?.name || "N/A"}</b>
+            Playing{" "}
+            <b
+              style={{
+                color: currentSong
+                  ? `rgb(${currentSong?.color
+                      .split(",")
+                      .map((c) => parseInt(c) - 75)
+                      .join(",")})`
+                  : "black",
+              }}
+            >
+              {currentSong?.name || "N/A"}
+            </b>
           </h1>
           <div className="flex flex-col gap-15 items-center">
-            <HotKeyButtons hotkeysMap={hotkeysMap} currentAudioRef={currentAudioRef} />
+            <HotKeyButtons
+              hotkeysMap={hotkeysMap}
+              currentAudioRef={currentAudioRef}
+            />
             <h1>
               Total Time Played:{" "}
               {getReadableDurationSong(currentSong?.secondsPlayed || 0)}
@@ -226,74 +266,71 @@ function CurrentSongInfo({
   );
 }
 
-function HotKeyButtons({hotkeysMap, currentAudioRef}) {
-  return <div className="flex flex-row gap-5 *:hover:scale-105 *:border *:p-1 *:rounded-xl *:bg-white/20 *:active:scale-95 *:select-none">
-    <h1
-      className="cursor-pointer"
-      title="Toggle Playing"
-      onClick={(e) => hotkeysMap[" "](e, currentAudioRef.current)}
-    >
-      {currentAudioRef?.current?.paused ? (
-        <Play />
-      ) : (
-        <Pause />
-      )}
-    </h1>
+function HotKeyButtons({ hotkeysMap, currentAudioRef }) {
+  return (
+    <div className="flex flex-row gap-5 *:hover:scale-105 *:border *:p-1 *:rounded-xl *:bg-white/20 *:active:scale-95 *:select-none">
+      <h1
+        className="cursor-pointer"
+        title="Toggle Playing"
+        onClick={(e) => hotkeysMap[" "](e, currentAudioRef.current)}
+      >
+        {currentAudioRef?.current?.paused ? <Play /> : <Pause />}
+      </h1>
 
-    <h1
-      className="cursor-pointer"
-      title="Toggle Loop"
-      onClick={(e) => {
-        e.preventDefault()
-        hotkeysMap["l"](e, currentAudioRef.current);
-      } }
-    >
-      {currentAudioRef?.current?.loop ? <Repeat /> : <RepeatOff />}
-    </h1>
+      <h1
+        className="cursor-pointer"
+        title="Toggle Loop"
+        onClick={(e) => {
+          e.preventDefault();
+          hotkeysMap["l"](e, currentAudioRef.current);
+        }}
+      >
+        {currentAudioRef?.current?.loop ? <Repeat /> : <RepeatOff />}
+      </h1>
 
-    <h1
-      className="cursor-pointer"
-      title="Start From Beginning"
-      onClick={(e) => {
-        e.preventDefault()
-        hotkeysMap[0](e, currentAudioRef.current);
-      } }
-    >
-      <SkipBack />
-    </h1>
+      <h1
+        className="cursor-pointer"
+        title="Start From Beginning"
+        onClick={(e) => {
+          e.preventDefault();
+          hotkeysMap[0](e, currentAudioRef.current);
+        }}
+      >
+        <SkipBack />
+      </h1>
 
-    <h1
-      className="cursor-pointer"
-      title="Play Next Song"
-      onClick={(e) => {
-        e.preventDefault()
-        hotkeysMap["s"](e, currentAudioRef.current);
-      } }
-    >
-      <SkipForward />
-    </h1>
+      <h1
+        className="cursor-pointer"
+        title="Play Next Song"
+        onClick={(e) => {
+          e.preventDefault();
+          hotkeysMap["s"](e, currentAudioRef.current);
+        }}
+      >
+        <SkipForward />
+      </h1>
 
-    <h1
-      className="cursor-pointer"
-      title="Skip Backward"
-      onClick={(e) => {
-        e.preventDefault()
-        hotkeysMap["ArrowLeft"](e, currentAudioRef.current);
-      } }
-    >
-      <ArrowLeft />
-    </h1>
+      <h1
+        className="cursor-pointer"
+        title="Skip Backward"
+        onClick={(e) => {
+          e.preventDefault();
+          hotkeysMap["ArrowLeft"](e, currentAudioRef.current);
+        }}
+      >
+        <ArrowLeft />
+      </h1>
 
-    <h1
-      className="cursor-pointer"
-      title="Skip Forward"
-      onClick={(e) => {
-        e.preventDefault()
-        hotkeysMap["ArrowRight"](e, currentAudioRef.current);
-      } }
-    >
-      <ArrowRight />
-    </h1>
-  </div>;
+      <h1
+        className="cursor-pointer"
+        title="Skip Forward"
+        onClick={(e) => {
+          e.preventDefault();
+          hotkeysMap["ArrowRight"](e, currentAudioRef.current);
+        }}
+      >
+        <ArrowRight />
+      </h1>
+    </div>
+  );
 }
-
