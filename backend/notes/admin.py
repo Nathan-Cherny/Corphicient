56a2ff3be6846aea10735194cca23556d3a61e8d
Nathan Cherny <natec3632@gmail.com>
2026-09-06 11:09:39 -0400

    see all this in admin

--- backend/notes/admin.py
+++ backend/notes/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
+
 from .models import *
+
 
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ('name', 'note',)
+    list_display = ('name', 'note')
     search_fields = ('name',)
+
+
+class SectionNoteInline(admin.TabularInline):
+    model = SectionNote
+    extra = 0
+    ordering = ('order',)
+    fields = ('note', 'order')
+
 
 @admin.register(Section)
 class SectionAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
+    inlines = [SectionNoteInline]
